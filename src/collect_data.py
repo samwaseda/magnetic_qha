b09a621817066436eb5d29d5fda3165a98827643
Sam Waseda <o.waseda@mpie.de>
2025-08-12 10:39:09 +0200

    implement collect_data

---
+++ src/collect_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+from collections import defaultdict
+from scipy.spatial import cKDTree
+from pyiron_base import PythonTemplateJob
+
+
+def get_job_table(pr):
+    df = pr.job_table()
+    df = df[(df.status != "running") & (df.status != "submitted")]
+    df["job_stem"] = ["_".join(job_name.split("_")[:2]) for job_name in df.job]
+    return df[~df.job_stem.duplicated(keep="last")]
+
+
+def _repeat(repeat, arr):
+    return np.array(repeat.prod() * arr.tolist())
+
+
+class CollectData(PythonTemplateJob):
+    def run_static(self):
+        data_dict = {"fcc": defaultdict(list), "bcc": defaultdict(list)}
+        df = get_job_table(self.project)
+        for job_name in df.job:
+            job = self.project.inspect(job_name)
+            structure = job["input/structure"].to_object()
+            repeat = 8 // (
+                structure.analyse.get_layers(distance_threshold=0.1).max(axis=0) + 1
+            )
+            E = job["output/generic/energy_pot"][-1] * repeat.prod()
+            try:
+                m = job["output/generic/dft"]["atom_spins"][-1]
+            except KeyError:
+                m = np.zeros(len(structure))
+            try:
+                f = job["output/generic/forces"][-1]
+                cs = job_name.split("_")[0]
+                data_dict[cs]["positions"].append(structure.repeat(repeat).positions)
+                data_dict[cs]["cell"].append(structure.repeat(repeat).cell)
+                data_dict[cs]["energy"].append(E)
+                data_dict[cs]["magmoms"].append(_repeat(repeat, m))
+                data_dict[cs]["forces"].append(_repeat(repeat, f))
+            except TypeError:
+                pass
+        for cs in ["fcc", "bcc"]:
+            for key, value in data_dict[cs].items():
+                data_dict[cs][key] = np.array(value)
+
+        for cs in ["fcc", "bcc"]:
+            data_dict[cs]["structure"] = self.project.create.structure.atoms(
+                elements=len(data_dict[cs]["positions"][0]) * ["Fe"],
+                positions=data_dict[cs]["positions"][0],
+                pbc=True,
+                cell=data_dict[cs]["cell"][0],
+            )
+            x = np.einsum(
+                "nji,nmj->nmi",
+                np.linalg.inv(data_dict[cs]["cell"]),
+                data_dict[cs]["positions"],
+            )
+            tree = cKDTree(x[0])
+            indices = np.array([tree.query(xx)[1] for xx in x])
+            data_dict[cs]["positions"] = np.array(
+                [xx[np.argsort(ii)] for xx, ii in zip(x, indices)]
+            )
+            data_dict[cs]["forces"] = np.array(
+                [f[np.argsort(ii)] for f, ii in zip(data_dict[cs]["forces"], indices)]
+            )
+            data_dict[cs]["magmoms"] = np.array(
+                [f[np.argsort(ii)] for f, ii in zip(data_dict[cs]["magmoms"], indices)]
+            )
+        self.output.data_dict = data_dict
+        self.status.finished = True
+        self.to_hdf()
